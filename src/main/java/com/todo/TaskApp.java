589313b84a4f55aaf15611e07bd870d1064a2bce
Benny Gil Lactaotao <122024352+benny-gil@users.noreply.github.com>
2023-12-31 12:59:00 +0800

    Saves tasks locally

--- src/main/java/com/todo/TaskApp.java
+++ src/main/java/com/todo/TaskApp.java
@@ -1,5 +1,10 @@
 package com.todo;
 
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
@@ -8,11 +13,13 @@ import java.awt.event.ActionListener;
 public class TaskApp {
     private DefaultListModel<String> listModel;
     private JList<String> taskList;
+    private static final String TASKS_FILE = "tasklist/tasks.txt";
+
 
     public TaskApp() {
         JFrame frame = new JFrame("Task App");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(400, 500);
+        frame.setSize(400, 300);
         frame.setLayout(new FlowLayout());
         frame.setLocationRelativeTo(null);
 
@@ -26,6 +33,7 @@ public class TaskApp {
                 String task = JOptionPane.showInputDialog(frame, "Enter Task");
                 if (task != null && !task.trim().isEmpty()) {
                     listModel.addElement(task);
+                    saveTasks();
                 }
             }
         });
@@ -38,6 +46,7 @@ public class TaskApp {
                 if (selectedIndex != -1) {
                     listModel.remove(selectedIndex);
                 }
+                saveTasks();
             }
         });
 
@@ -52,6 +61,7 @@ public class TaskApp {
                         listModel.set(selectedIndex, task);
                     }
                 }
+                saveTasks();
             }
         });
 
@@ -60,6 +70,33 @@ public class TaskApp {
         frame.add(removeButton);
         frame.add(editButton);
         frame.setVisible(true);
+        loadTasks();
+    }
+    private void saveTasks() {
+        try (BufferedWriter writer = Files.newBufferedWriter(Paths.get(TASKS_FILE))) {
+            for (int i = 0; i < listModel.getSize(); i++) {
+                writer.write(listModel.getElementAt(i));
+                writer.newLine();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void loadTasks() {
+        Path path = Paths.get(TASKS_FILE);
+        if (!Files.exists(path)) {
+            return;
+        }
+
+        try {
+            List<String> lines = Files.readAllLines(path);
+            for (String line : lines) {
+                listModel.addElement(line);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public static void main(String[] args) {
